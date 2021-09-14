6666f2442fa8ca76452a71feedabb5441c4e9dc4
Sávio Cordeiro <saviocord@gmail.com>
2021-09-13 21:46:15 -0300

    configure enum in project

---
+++ app/helpers/enum_i18n_helper.rb
@@ -0,0 +1,26 @@
+# frozen_string_literal: true
+
+module EnumI18nHelper
+  # Returns an array of the possible key/i18n values for the enum
+  # Example usage:
+  # enum_options_for_select(User, :approval_state)
+  def enum_options_for_select(class_name, enum)
+    class_name.send(enum.to_s.pluralize).map do |key, _|
+      [enum_i18n(class_name, enum, key), key]
+    end
+  end
+
+  # Returns the i18n version the models current enum key
+  # Example usage:
+  # enum_l(user, :approval_state)
+  def enum_l(model, enum)
+    enum_i18n(model.class, enum, model.send(enum))
+  end
+
+  # Returns the i18n string for the enum key
+  # Example usage:
+  # enum_i18n(User, :approval_state, :unprocessed)
+  def enum_i18n(class_name, enum, key)
+    I18n.t("#{class_name.model_name.to_s.underscore.pluralize}.enums.#{enum.to_s.pluralize}.#{key}")
+  end
+end
--- app/models/project.rb
+++ app/models/project.rb
@@ -4,4 +4,6 @@ class Project < ApplicationRecord
   has_and_belongs_to_many :users
 
   validates :name, :enable, :visibility, presence: true
+
+  enum visibility: { public_view: 0, private_view: 1 }
 end
